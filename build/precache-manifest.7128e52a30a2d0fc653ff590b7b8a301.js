34f89a76914984a65dafd8322de83dfd4a0b7e88
rebecaalbuquerque <rebeca.albuquerque96@gmail.com>
2020-10-12 14:24:43 -0300

    Config uploading

---
+++ build/precache-manifest.7128e52a30a2d0fc653ff590b7b8a301.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a9416261882400b064adebd4d15ab2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "863a234cf6bc769230ed",
+    "url": "/static/css/main.c6304252.chunk.css"
+  },
+  {
+    "revision": "1b93992e6c974ac3b04c",
+    "url": "/static/js/2.35ad3ffc.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/static/js/2.35ad3ffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "863a234cf6bc769230ed",
+    "url": "/static/js/main.d258cd0a.chunk.js"
+  },
+  {
+    "revision": "8b1bd5583c708f8af1c8d7193507764f",
+    "url": "/static/js/main.d258cd0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9693c7216805218aa318",
+    "url": "/static/js/runtime-main.09420661.js"
+  },
+  {
+    "revision": "8224e0160e362e117cbe00495919e2af",
+    "url": "/static/media/nucleo-icons.8224e016.eot"
+  },
+  {
+    "revision": "861720fa94b80958075f620b2c6e8fb9",
+    "url": "/static/media/nucleo-icons.861720fa.woff"
+  },
+  {
+    "revision": "ae42fa524981a56f3f289447cff36eb1",
+    "url": "/static/media/nucleo-icons.ae42fa52.svg"
+  },
+  {
+    "revision": "b0dc05d015e91e7d28d79cd0056fe555",
+    "url": "/static/media/nucleo-icons.b0dc05d0.ttf"
+  },
+  {
+    "revision": "ff208b8d733e61a630992ee2b72c9c1c",
+    "url": "/static/media/nucleo-icons.ff208b8d.woff2"
+  }
+]);
